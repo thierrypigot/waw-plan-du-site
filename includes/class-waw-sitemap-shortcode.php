24bb3c98a20dd421e57c2a6eb70ef958185067f1
thierrypigot <contact@wearewp.pro>
2026-07-31 18:03:12 +0200

    feat: shortcode waw_sitemap_page

--- includes/class-waw-sitemap-shortcode.php
+++ includes/class-waw-sitemap-shortcode.php
@@ -1,2 +1,23 @@
 <?php
+/**
+ * Shortcode [waw_sitemap_page].
+ */
+
 defined( 'ABSPATH' ) || exit;
+
+class WAW_Sitemap_Shortcode {
+
+	public static function register(): void {
+		add_shortcode( 'waw_sitemap_page', array( __CLASS__, 'render' ) );
+	}
+
+	/**
+	 * La normalisation (défauts, whitelist) est déléguée au renderer,
+	 * partagée avec le futur bloc Gutenberg.
+	 *
+	 * @param array|string $atts Attributs du shortcode ('' si aucun).
+	 */
+	public static function render( $atts ): string {
+		return WAW_Sitemap_Renderer::render( is_array( $atts ) ? $atts : array() );
+	}
+}
